cee795385b0689f36c07f7091d547026a0d7dc8f
Roderick Zambrano <roderick.zambrano.g@gmail.com>
2022-10-09 23:34:09 -0500

    Updated portainer init shell script to remove existing portainer container when stopping container in a hard reset.

--- linux/docker-compose/portainer/portainer_init.sh
+++ linux/docker-compose/portainer/portainer_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker volume create portainer_data # Without volume creation, docker run will fail.
+docker rm portainer # Remove previous instance of portainer if any
 # Docker compose makes portainer lose authority over its own container. `docker run` exec avoids this.
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     --restart=always \
